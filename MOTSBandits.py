Fixed bug with the calculation of the CI in the Pareto UCB algorithm

MOTSBandits.py
@@ -132,7 +132,7 @@ class ParetoUCB1Bandit:
         """
         if np.all(self.arm_counts > 0):
             ucb_values = self.arm_means + self.kappa * np.sqrt(
-                (2 * math.log(self.n * pow(self.num_objectives, 1 / 4))) / self.arm_counts
+                (2 * math.log(self.n * pow(self.num_objectives * self.num_arms, 1 / 4))) / self.arm_counts
             )
             is_strictly_worse = np.all(ucb_values[:, None, :] < ucb_values[None, :, :], axis=2)
             pareto_indices = np.where(~np.any(is_strictly_worse, axis=1))[0]