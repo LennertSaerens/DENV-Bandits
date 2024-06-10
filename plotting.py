506fe38284bc9a734b95c5aae979a1fafc43d0db
LennertSaerens <lennert.saerens@gmail.com>
2024-06-10 17:15:07 +0200

    Updated plotting code to allow for comparison between bandits.

--- plotting.py
+++ plotting.py
@@ -203,18 +203,17 @@ def plot_bernoulli_metric(setup):
     Plot the evolution of the Bernoulli metric for a single algorithm in the experimental setup. The Bernoulli metric is the fraction of times the algorithm pulls a Pareto optimal arm.
     The x-axis represents the time steps and the y-axis represents the Bernoulli metric. The metric is averaged over the experiments.
     :param setup: The experimental setup dictionary.
-    :param algorithm_name: The name of the algorithm to plot.
     :return: None
     """
     for algorithm_name in setup:
         bernoulli_metrics = setup[algorithm_name]["cor_rec_ber"]
         avg_bernoulli_metrics = np.mean(bernoulli_metrics, axis=0)
         plt.plot(avg_bernoulli_metrics, label=f"{algorithm_name}")
-        plt.title(f"Bernoulli metric for {algorithm_name}")
-        plt.xlabel("Time steps")
-        plt.ylabel("Bernoulli metric")
-        plt.legend()
-        plt.show()
+    plt.title(f"Bernoulli metric")
+    plt.xlabel("Time steps")
+    plt.ylabel("Bernoulli metric")
+    plt.legend()
+    plt.show()
 
 
 def plot_jaccard_metric(setup):
@@ -233,11 +232,11 @@ def plot_jaccard_metric(setup):
                          avg_jaccard_metrics - 1.96 * std_jaccard_metrics / np.sqrt(len(jaccard_metrics)),
                          avg_jaccard_metrics + 1.96 * std_jaccard_metrics / np.sqrt(len(jaccard_metrics)),
                          alpha=0.2)
-        plt.title(f"Jaccard similarity metric for {algorithm_name}")
-        plt.xlabel("Time steps")
-        plt.ylabel("Jaccard metric")
-        plt.legend()
-        plt.show()
+    plt.title(f"Jaccard similarity metric")
+    plt.xlabel("Time steps")
+    plt.ylabel("Jaccard metric")
+    plt.legend()
+    plt.show()
 
 
 def plot_hypervolume(setup):
@@ -251,8 +250,8 @@ def plot_hypervolume(setup):
         hypervolumes = setup[algorithm_name]["hypervolume"]
         avg_hypervolumes = np.mean(hypervolumes, axis=0)
         plt.plot(avg_hypervolumes, label=f"{algorithm_name}")
-        plt.title(f"Hypervolume metric for {algorithm_name}")
-        plt.xlabel("Time steps")
-        plt.ylabel("Hypervolume")
-        plt.legend()
-        plt.show()
+    plt.title(f"Hypervolume metric")
+    plt.xlabel("Time steps")
+    plt.ylabel("Hypervolume")
+    plt.legend()
+    plt.show()
