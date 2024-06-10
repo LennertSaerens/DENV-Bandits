e5bb3b9e0c39e0273518bb11ddc6107f4b3b1388
LennertSaerens <lennert.saerens@gmail.com>
2024-06-10 16:59:43 +0200

    Deleted redundant versions of arm recommendation method for the different MP-TS variants

--- bandits/ThompsonSampling.py
+++ bandits/ThompsonSampling.py
@@ -58,14 +58,6 @@ class PTSBandit:
         self.alphas = np.ones((self.num_arms, self.num_objectives))
         self.betas = np.ones((self.num_arms, self.num_objectives))
 
-    def get_top_arms(self, n):
-        """
-        Get the top n arms based on the mean rewards.
-        :param n: The number of top arms to return.
-        :return: The indices of the top n arms.
-        """
-        return np.argsort(np.mean(self.alphas / (self.alphas + self.betas), axis=1))[-n:]
-
 
 class LSTSBandit:
     """
@@ -117,14 +109,6 @@ class LSTSBandit:
         self.alphas = np.ones((self.num_scalarization_functions, self.num_arms, self.num_objectives))
         self.betas = np.ones((self.num_scalarization_functions, self.num_arms, self.num_objectives))
 
-    def get_top_arms(self, n):
-        """
-        Get the top n arms based on the mean rewards.
-        :param n: The number of top arms to return.
-        :return: The indices of the top n arms.
-        """
-        return np.argsort(np.sum(self.alphas[self.MRU] / (self.alphas[self.MRU] + self.betas[self.MRU]), axis=1))[-n:]
-
 
 class NormalPTSBandit:
     """
@@ -174,14 +158,6 @@ class NormalPTSBandit:
         self.precision = np.ones((self.num_arms, self.num_objectives))
         self.alpha = np.ones((self.num_arms, self.num_objectives))
         self.beta = np.ones((self.num_arms, self.num_objectives))
-
-    def get_top_arms(self, n):
-        """
-        Get the top n arms based on the mean rewards.
-        :param n: The number of top arms to return.
-        :return: The indices of the top n arms.
-        """
-        return np.argsort(np.mean(self.mu, axis=1))[-n:]
 
 
 class NormalLSTSBandit:
@@ -243,11 +219,3 @@ class NormalLSTSBandit:
         self.precision = np.ones((self.num_scalarization_functions, self.num_arms, self.num_objectives))
         self.alpha = np.ones((self.num_scalarization_functions, self.num_arms, self.num_objectives))
         self.beta = np.ones((self.num_scalarization_functions, self.num_arms, self.num_objectives))
-
-    def get_top_arms(self, n):
-        """
-        Get the top n arms based on the mean rewards.
-        :param n: The number of top arms to return.
-        :return: The indices of the top n arms.
-        """
-        return np.argsort(np.mean(self.mu[self.MRU], axis=1))[-n:]
